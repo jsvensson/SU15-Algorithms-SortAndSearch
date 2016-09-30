b42c042706bd610df826362fc16d4db6e7928b10
Johan Svensson <johan@atomicplayboy.net>
2016-10-01 00:16:06 +0200

    Initial commit

---
+++ Benchmark/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Sorting;
+
+namespace Benchmark
+{
+    class Program
+    {
+        private const int Loops = 100000;
+        private readonly int[] intArray = GenerateSortedArray(Loops);
+        private readonly int[] arraySizes = {1024, 2048, 4096, 16384, 32768};
+        private readonly Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            var searchMethods = new List<Func<int[], int, bool>>
+            {
+                Search.Linear
+            };
+
+            var tests = new Program();
+
+            foreach (var search in searchMethods)
+            {
+                foreach (var testSize in tests.arraySizes)
+                {
+                    Stopwatch stopWatch = Stopwatch.StartNew();
+
+                    for (int i = 0; i < Loops; i++)
+                    {
+                        int target = tests.rnd.Next(Loops);
+                        search(tests.intArray, target);
+                    }
+                    stopWatch.Stop();
+                    Console.WriteLine($"Size {testSize}: {stopWatch.ElapsedMilliseconds}ms");
+                }
+            }
+
+        }
+
+        private static int[] GenerateSortedArray(int length)
+        {
+            return Enumerable.Range(0, length).ToArray();
+        }
+
+        public static void BenchmarkSearch(Func<int[], int, bool> searchMethod)
+        {
+            
+        }
+    }
+}
---
+++ Sorting/Search.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public static class Search
+    {
+        public static bool Linear(int[] array, int target)
+        {
+            foreach (int t in array)
+            {
+                if (t == target)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
